068bd08d5969b8c9d76417473014afc42c45eb66
Gabe Kuslansky <kuslanskyg@gmail.com>
2023-02-04 02:13:12 -0500

    make spotify green

--- src/components/SpotifyLoginButton.tsx
+++ src/components/SpotifyLoginButton.tsx
@@ -1,4 +1,3 @@
-import { useTheme } from "@emotion/react";
 import { Button, useMantineColorScheme, useMantineTheme } from "@mantine/core";
 import { IconBrandSpotify } from "@tabler/icons";
 import getConfig from "next/config";
@@ -9,9 +8,9 @@ export default function SpotifyLoginButton({ redirectToApp = false }) {
     publicRuntimeConfig: { BACKEND_URL },
   } = getConfig();
 
-  const { colors: { spotifyGreen, spotifyBlack } } = useMantineTheme();
+  const { colors: { spotifyGreen, spotifyBlack} } = useMantineTheme();
 
   const getColor = (color: 'green' | 'black') => color === 'green' ? spotifyGreen[0] : spotifyBlack[0]
 
-  return <Button bg={getColor('black')} fullWidth onClick={() => { window.location.href = `${BACKEND_URL}/auth/login?redirectToApp=${redirectToApp}` }} variant="outline" color="spotifyGreen.1"><IconBrandSpotify style={{ marginRight: '5px' }} />Login with Spotify</Button>
+  return <Button bg={getColor('black')} fullWidth onClick={() => { window.location.href = `${BACKEND_URL}/auth/login?redirectToApp=${redirectToApp}` }}  variant="outline" styles={theme => ({'root': {'color': getColor('green'), borderColor: getColor('green')}})}><IconBrandSpotify style={{ marginRight: '5px' }} />Login with Spotify</Button>
 } 
