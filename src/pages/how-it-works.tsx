0e069c6982fa94053dd4867d5882ef5af246518a
Gabe Kuslansky <kuslanskyg@gmail.com>
2023-10-03 12:21:00 -0400

    adjust inline logo height

--- src/pages/how-it-works.tsx
+++ src/pages/how-it-works.tsx
@@ -6,15 +6,15 @@ export default function HowItWorksPage() {
     const { classes } = useStyles();
     return (
         <Container style={{ display: 'flex', flexDirection: 'column', flexWrap: 'wrap' }}>
-            <Container size={"lg"} className={classes.textContainer} mt={'xs'}>
-                <Card>
+            <Container w='130%' size={"lg"} className={classes.textContainer} mt={'xs'}>
+                <Card >
                     <Title mt={2}>
                         INTERVIEW YOUR ELDER
                     </Title>
                     <Text mt={'xs'}>Sit down with your elder and do your simple interview. Record their answers and let the app do the rest! Use this time to learn about yourself and your elder!</Text>
                 </Card>
             </Container>
-            <Container size={"lg"} className={classes.textContainer} mt={'xs'}>
+            <Container w='130%' size={"lg"} className={classes.textContainer} mt={'xs'}>
                 <Card>
                     <Title mt={2}>
                         REVIEW THE SONG-LIST
@@ -22,14 +22,15 @@ export default function HowItWorksPage() {
                     <Text mt={'xs'}>{"Your answers magically create a list of songs from your elder's life. You can listen to the songs and edit the list, and share the list with family."}</Text>
                 </Card>
             </Container>
-            <Container size={"lg"} className={classes.textContainer} mt={'xs'}>
+            <Container w='150%' size={"lg"} className={classes.textContainer} mt={'xs'}>
                 <Card>
-                    <Title style={{ display: 'flex', alignContent: 'center' }} mt={2}>
-                        SAVE TO <Image style={{ paddingLeft: '10px', width: '160px', display: 'inline-block', lineHeight: 0 }} src={SpotifyLogo.src} />
+                    <Title style={{ display: 'flex', alignItems: 'center' }} mt={2}>
+                        SAVE TO <Image style={{ paddingLeft: '10px', width: '160px', }} src={SpotifyLogo.src} />
                     </Title>
-                    <Text mt={'xs'}>You can save your playlist to <Image style={{ paddingLeft: '10px', width: '90px', display: 'inline-block', lineHeight: 0 }} src={SpotifyLogo.src} /> for easy access and editing. With a Premium <Image style={{ paddingLeft: '10px', width: '90px', display: 'inline-block', lineHeight: 0 }} src={SpotifyLogo.src} /> account, you can access the music offline.</Text>
+                    <Text style={{ display: 'flex', alignItems: 'center' }} mt={'xs'}><Text w='225px'>
+                        You can save your playlist to</Text> <Image mr='5px' style={{ width: '95px' }} src={SpotifyLogo.src} /> <Text style={{ display: 'flex', alignContent: 'center' }} w='350px'>for easy access and editing. With a Premium </Text><Image style={{ width: '95px', }} mr='5px' src={SpotifyLogo.src} /> <Text w='400px' style={{ display: 'flex', alignContent: 'center' }}>account, you can access the music offline.</Text></Text>
                 </Card>
-            </Container>
-        </Container>
+            </Container >
+        </Container >
     )
 }
